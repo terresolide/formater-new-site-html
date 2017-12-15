66807b3a499473fbbcf2f967d0b1db9d9cea9195
epointal <elisabeth.pointal@free.fr>
2017-12-15 17:03:12 +0100

    language for page

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <?php 
 /**
+ * Redirection page 
 */
 
+
+
+
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+ $lang = $lang == "fr" ? "fr": "en";
+
+$language = array(
+    "title" => array( 
+        "fr" => "ForM@Ter a un nouveau site",
+        "en" => "A new portal for ForM@Ter")
+);
+function printer( $name){
+    global $lang, $language;
+    if( isset( $language[ $name ]) && isset( $language[ $name ][ $lang])){
+        echo $language[$name][$lang];
+    }
+}
 
 ?><!DOCTYPE html>
 <html>
@@ -9,6 +27,7 @@
     <title>Page site change</title>
   </head>
   <body>
+  <h1><?php printer( "title");?></h1>
   Cette page est une
   page toute simple
   </body>
